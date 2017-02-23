dc405f349aeba808c338c633830cc138e7df5e63
sam erie <serie@alaska.edu>
2017-02-23 02:33:24 -0900

    see last commit, didn't add all changes

--- GFXlib.cpp
+++ GFXlib.cpp
@@ -37,75 +37,22 @@ void defaultDraw()
 	glutPostRedisplay();
 }
 
+//both classes badly need refactor
+//need destructors, memory leaks aplenty!
+
 class display	//this will create display and start glut loop
 {				//must figure out how to pass functions to display
-public:
-
-	display()	//should this take title? or should it get set function like res, functions, etc will
-	{
-//		timerEvent = &defaultTimer;	//namespace problem? I don't get it
-		title = "My GFX lib";
-	}
-
-	int winHeight = 3000, winWidth = 2000;	//need to find out how to init these for diff machines
-	int refresh = 20;	//need to make sure this does what i think it does
-	string title;
-
-	void (*timerEvent)(int) = &defaultTimer;
-	void (*draw)() = &defaultDraw;
-	void(*keyboard)(unsigned char, int, int) = &defaultKeyboard;
-	
-
-	
-
-public:
-
-	void openDisplay(int *ac, char ** av)	//need to find out what these command line params are used for
-	{
-
-		glutInit(ac, av);	//at_EXIT_HACK for windows, research
-
-		glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);	//is there any reason to change these
-
-		glutInitWindowSize(winWidth, winHeight);
-
-		glutCreateWindow(title.c_str());
-
-		glutDisplayFunc(defaultDraw);
-		glutKeyboardFunc(defaultKeyboard);
-		glutTimerFunc(refresh, timerEvent, 0);
-
-		//how much harder would it to be to access OpenGL without freeglut and glew?
-		//if I'm piggybacking on these libraries I have to accept all glutxxxxFunc binds
-
-		glClearColor(0.f, 0.f, 0.f, 0.f);	//this needs to take variables
-		glViewport(0, 0, winWidth, winHeight);	//this needs more customizable, maybe...prob not for 2d
-
-		glMatrixMode(GL_PROJECTION);	//again prob good for 2d
-		glLoadIdentity();
-
-		//Init GLEW
-		//is glew needed without 3d objects?
-		gluPerspective(60.f, (GLfloat)winWidth / winHeight, 0.f, 50.f);
-	//	glewInit();
-
-		//run glut loop
-		glutMainLoop();	
-
-
-
-	}
 
 	class texture	//this will load textures that can be drawn to display
 	{				//should this hold shader program or display?
-		
-		//shader program functions
+
+					//shader program functions
 		GLuint compileShader(GLenum shaderType, const string source)
 		{
 			GLuint shader = glCreateShader(shaderType);
 
 			const char * sourceArray[1] = { source.c_str() };	//needs to match const GLchar *
-			glShaderSource(shader, 1, sourceArray, NULL);		
+			glShaderSource(shader, 1, sourceArray, NULL);
 			glCompileShader(shader);
 
 			GLint compileResults;
@@ -118,14 +65,15 @@ public:
 				//see other project for how to log
 			}
 
+			return shader;
 		}
-		
-		
+
+
 		GLuint compileShaderProgram(const string &vsSource, const string &fsSource)
 		{
 			GLuint program = glCreateProgram();
 
-			if (program == 0) 
+			if (program == 0)
 			{
 				//should probably throw program creation failed error
 			}
@@ -183,34 +131,199 @@ public:
 		const string fs = R"(
 			precision mediump float;
 			varying vec2 vCoord;
-			uniform sampler2D sTex;
+			uniform sampler2D uTex;
 
 			void main()
 			{
-				gl_FragColor = texture2D(sTex, vCoord);	
+				gl_FragColor = texture2D(uTex, vCoord);	
 			}	
 		)";
 		//need to do a lot of refactoring, put stuff in header once this works
 		GLuint _program;
+
+		GLuint _texture;
+		GLuint _samplerLocation;
+		GLuint _textureCoord;	//location or coordinates?
+
 		GLuint _positionAttribute;
 		GLuint _colorAttribute;
-		GLuint _modelUniform;
+
+		/*	GLuint _modelUniform;
 		GLuint _viewUniform;
-		GLuint _projUniform;
+		GLuint _projUniform;*/
 		int texWidth, texHeight;
 		//heres where the magic happens
-	public:
-		texture(const string &file)
+
+		//again really need to refactor, here is the vert and ind to draw texture on
+		//need to fix these values to relate to resizing image, once i get this working
+		//I am combining code from my summer project
+		GLfloat h = 3.0f;
+		GLfloat r = 3.0f;
+		GLfloat w = 0.75f;
+		GLfloat b = 1.0f;
+		GLfloat Vertices[20] =
 		{
+			-h, w, 0.0f,
+			0.0f, b,
+			-h, -w, 0.0f,
+			0.0f, 0.0f,
+			r, -w, 0.0f,
+			b, 0.0f,
+			r, w, 0.0f,
+			b, b
+		};
+		GLushort Indices[6] = { 0, 1, 2 ,0, 2, 3 };
+
+
+	public:
+
+		texture() {}
+
+		void load(const string &file)
+		{
+			//set up shaders
+
+			//this needs error checking
 			_program = compileShaderProgram(vs, fs);
-			_positionAttribute = glGetAttribLocation(_program, "aPosition");
+			_positionAttribute = glGetAttribLocation(_program, "aPos");
+			_textureCoord = glGetAttribLocation(_program, "aCoord");
+			_samplerLocation = glGetUniformLocation(_program, "uTex");	//these names need fixed, not descriptive
 			_colorAttribute = glGetAttribLocation(_program, "aColor");
-			_modelUniform = glGetUniformLocation(_program, "uModelMatrix");
+			/*	_modelUniform = glGetUniformLocation(_program, "uModelMatrix");
 			_viewUniform = glGetUniformLocation(_program, "uViewMatrix");
-			_projUniform = glGetUniformLocation(_program, "uProjMatrix");
+			_projUniform = glGetUniformLocation(_program, "uProjMatrix");*/
+
+			//load the texture from file
+
+			//opengl part
+			glGenTextures(1, &_texture);
+			glBindTexture(GL_TEXTURE_2D, _texture);
+			//stb_image.h part
+			loadImage(file);
 		}
 
+		void drawTexture()
+		{
+			glUseProgram(_program);
+			glVertexAttribPointer(_positionAttribute, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), Vertices);
+			glVertexAttribPointer(_textureCoord, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), Vertices + 3);
+
+			glEnableVertexAttribArray(_positionAttribute);
+			glEnableVertexAttribArray(_textureCoord);
+
+			glActiveTexture(GL_TEXTURE0);	//this will be a conflict if drawing multiple textures
+											//needs to be planned for
+			glBindTexture(GL_TEXTURE_2D, _texture);
+
+			glUniform1i(_samplerLocation, 0);	//also this
+
+												//needed to overlap sprites - otherwise transparant background draw overwrites previously drawn background
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			/////////////////////////////////////////////////////
+
+			//this is the actual draw
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, Indices);
+
+
+		}
+
+	private:
+
+
+		//loads file using stbi_image free lib...needs error checking
+		void loadImage(const string &file)
+		{
+			int width = 0, height = 0, channels = 0;
+
+			unsigned char * img;
+
+			stbi_set_flip_vertically_on_load(true);
+			img = stbi_load(file.c_str(), &width, &height, &channels, STBI_rgb_alpha);
+
+			//save size to member variables
+			texWidth = width;
+			texHeight = height;
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
+		}
 	};
+
+public:
+
+	display()	//should this take title? or should it get set function like res, functions, etc will
+	{
+		glewInit();	//this is needed to load texture, i think
+		title = "My GFX lib";
+		test.load("test.jpg");
+	}
+
+	int winHeight = 3000, winWidth = 2000;	//need to find out how to init these for diff machines
+	int refresh = 20;	//need to make sure this does what i think it does
+	string title;
+
+	texture test;
+
+	void testDraw()
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+		test.drawTexture();
+
+		glutSwapBuffers();
+		glutPostRedisplay();
+	}
+
+	void (*timerEvent)(int) = &defaultTimer;
+	void (*draw)() = &defaultDraw;
+	void(*keyboard)(unsigned char, int, int) = &defaultKeyboard;
+	
+
+	
+
+public:
+
+	void openDisplay(int *ac, char ** av)	//need to find out what these command line params are used for
+	{
+
+		glutInit(ac, av);	//at_EXIT_HACK for windows, research
+
+		glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);	//is there any reason to change these
+
+		glutInitWindowSize(winWidth, winHeight);
+
+		glutCreateWindow(title.c_str());
+
+		glutDisplayFunc(defaultDraw);
+		glutKeyboardFunc(defaultKeyboard);
+		glutTimerFunc(refresh, timerEvent, 0);
+
+		//how much harder would it to be to access OpenGL without freeglut and glew?
+		//if I'm piggybacking on these libraries I have to accept all glutxxxxFunc binds
+
+		glClearColor(0.f, 0.f, 0.f, 0.f);	//this needs to take variables
+		glViewport(0, 0, winWidth, winHeight);	//this needs more customizable, maybe...prob not for 2d
+
+		glMatrixMode(GL_PROJECTION);	//again prob good for 2d
+		glLoadIdentity();
+
+		//Init GLEW
+		//is glew needed without 3d objects?
+		gluPerspective(60.f, (GLfloat)winWidth / winHeight, 0.f, 50.f);
+		
+
+		//run glut loop
+		glutMainLoop();	
+
+
+
+	}
+
+	
 };
 
 
